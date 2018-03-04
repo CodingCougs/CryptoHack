2795c337fb73e0f82dc22485b8d8f25d41c6c19d
Randy Tran <10tranr@gmail.com>
2018-03-04 01:33:43 -0800

    Fixed issue for android phones

--- cryptoHackApp/src/detailView/buySell.js
+++ cryptoHackApp/src/detailView/buySell.js
@@ -9,33 +9,22 @@ export default class BuySell extends Component {
     render() {
         return (
             <View style={styles.contianer}>
-                <Button style={styles.buttonStyle}
-                text='Buy'
-                buttonStyle={{
-                backgroundColor: '#52c45d'
-                }}
+                <Button
+                    text='Buy'
+                    buttonStyle={styles.buttonStyle}
                 />
-                <Button style={styles.buttonStyle}
-                text='Sell'
-                buttonStyle={{
-                    backgroundColor: '#52c45d'
-                }}
+                <Button
+                    text='Sell'
+                    buttonStyle={styles.buttonStyle}
                 />
             </View>
         );}
 }
 
 const styles = StyleSheet.create({
-    contianer: {
-        // flex: 1,
-        flexDirection: 'row',
-        alignItems:'center',
-        justifyContent:'center',
-        // flexWrap: 'wrap',
-        padding: 10
-    },
     buttonStyle:{
+        backgroundColor: '#52c45d',
         margin: 20,
         width: 100
-    }
+        }
 })
