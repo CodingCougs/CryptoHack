2f403fce94b14db11a415ae7f792712584376383
Randy Tran <10tranr@gmail.com>
2018-03-03 23:48:25 -0800

    fixed router and added action calls

--- cryptoHackApp/src/currencyList/_currencyList.js
+++ cryptoHackApp/src/currencyList/_currencyList.js
@@ -1,6 +1,7 @@
 import CurrencyList from './currencyList';
 import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import * as currencyListAction from './currencyList_actions';
 
 
 function mapStateToProps(state){
@@ -11,6 +12,11 @@ function mapStateToProps(state){
 
 function mapDispatchToProps(dispatch){
     return bindActionCreators({
+
+        getTopHundyCoins: currencyListAction.getTop100Coin,
+        getTopTenCoins: currencyListAction.getTop10Coin,
+        getCoinDayHistory: currencyListAction.getCoinDayHistory,
+        getCoinCurrentPrice: currencyListAction.getCoinCurrentPrice
 
     }, dispatch)
 }
